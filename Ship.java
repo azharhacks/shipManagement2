51c7ea41e1c2d4105451f9063a94e2bb4e65cda9
Alvin Kyalo <kyaloalviin@gmail.com>
2025-06-17 20:20:05 +0300

    Implement database methods for Ship class, including save, findById, and delete functionalities

--- Ship.java
+++ Ship.java
@@ -1,6 +1,12 @@
- //i will try to connect this class to the user class which will be the one to input the data
            //find a way to store the info inputed by the user to the database   
-     public abstract class Ship {        
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public abstract class Ship {   
+    protected int id; // Unique identifier for the ship
     protected String type;            
     protected String location;
     protected String destination;
@@ -11,14 +17,75 @@
         this.destination = destination;
     }
 
-    
-
-    // Common behavior
-    public void setLocation(String location) { this.location = location; }
-    public void setDestination(String destination) { this.destination = destination; }
-
+    //getters amd setters public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
     public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
     public String getDestination() { return destination; }
+    public void setDestination(String destination) { this.destination = destination; }
     public String getType() { return type; }
-}
 
+    //database methods
+    public void save(){
+        if (id == 0) {
+            // Insert new ship into the database
+            String sql = "INSERT INTO ships (type, location, destination) OUTPUT INSERTED.id VALUES (?, ?, ?)";
+            try (Connection conn = DatabaseConnection.getConnection();
+                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setString(1, type);
+                pstmt.setString(2, location);
+                pstmt.setString(3, destination);
+                
+                try (ResultSet rs = pstmt.executeQuery()) {
+                    if (rs.next()) {
+                        this.id = rs.getInt(1);
+                    }
+                }
+            } catch (SQLException e) {
+                System.err.println("Error saving ship: " + e.getMessage());
+                e.printStackTrace();
+            }
+        } else {
+            // Update existing ship
+            String sql = "UPDATE ships SET location = ?, destination = ? WHERE id = ?";
+            DatabaseHelper.executeUpdate(sql, location, destination, id);
+        }
+    }
+   public static Ship findById(int id) {
+        String sql = "SELECT * FROM ships WHERE id = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setInt(1, id);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    String type = rs.getString("type");
+                    String location = rs.getString("location");
+                    String destination = rs.getString("destination");
+                    
+                    Ship ship;
+                    if ("Cargo".equalsIgnoreCase(type)) {
+                        double cargoCapacity = rs.getDouble("cargo_capacity");
+                        ship = new CargoShip(type, location, destination, cargoCapacity);
+                    } else {
+                        int passengerCapacity = rs.getInt("passenger_capacity");
+                        ship = new PassengerShip(type, location, passengerCapacity);
+                    }
+                    ship.setId(id);
+                    return ship;
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Error finding ship: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void delete() {
+        if (id != 0) {
+            String sql = "DELETE FROM ships WHERE id = ?";
+            DatabaseHelper.executeUpdate(sql, id);
+            this.id = 0;
+        }
+    }
+}
