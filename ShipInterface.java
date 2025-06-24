2371fd815ca5e4efd197a1860a3eb18857b041b9
azharhacks <azharabeid5@gmail.com>
2025-06-24 11:09:33 +0300

    Initial commit on shipManagement-azhar branch

---
+++ ShipInterface.java
@@ -0,0 +1,7 @@
+public interface ShipInterface {
+    void setId(int id);
+    String getLocation();
+    void setLocation(String location);
+    String getDestination();
+    void setDestination(String destination);
+}
